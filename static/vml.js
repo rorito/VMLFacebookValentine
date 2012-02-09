061ef40780c2fb376e06524c5a1183d9154d6cc6
hscissors <hscissors@gmail.com>
2012-02-09 15:31:37 -0500

    updated

--- static/vml.js
+++ static/vml.js
@@ -1,63 +1,110 @@
 //var ROOT_URL = 'http://localhost:8080/'
 var ROOT_URL = 'http://vmlfbval.appspot.com';
-var VALENTINE = 'MOOOOOO';
-var MESSAGE = 'Hey random person, turns out you’re my Facebook Valentine! Whoever you are, you sure like my posts. And I like that. I could write "Stalker Alert" and you’d probably give it a double thumbs-up. Keep it up stranger (but stop going through my trash). Happy Valentine’s Day!Hey random person, turns out you’re my Facebook Valentine! Whoever you are, you sure like my posts. And I like that. I could write "Stalker Alert" and you’d probably give it a double thumbs-up. Keep it up stranger (but stop going through my trash). Happy Valentine’s Day!';
+var VALENTINE = '';
+var MESSAGE = '';
+var DEBUG = false;
+var NO_LIKES = false;
+
+var LIKE_DELAY = 750;
+var FADEIN_DELAY = 200;
+var NO_LIKE_DEALY = 5000;
+
+var TEST;
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '374030012622491', // App ID
+      channelUrl : 'http://vmlfbval.appspot.com/channel.html', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true,  // parse XFBML
+      oauth      : true
+    });
+
+    FB.Event.subscribe('auth.statusChange', fbLoginStatus);
+};
+
+(function(d){
+    var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+    js = d.createElement('script'); js.id = id; js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    d.getElementsByTagName('head')[0].appendChild(js);
+}(document));
 
 $(document).ready(function(){
-	console.log("Doc ready!")
-	var tag = document.createElement('script');
- 	tag.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
- 	document.getElementById('fb-root').appendChild(tag);
-    
-    $('.facebook-message').click(chooseMessage);
-    $('.facebook-send').click(contactValentine);
+    //setup button listeners
+    $('.facebook-continue').click(function(){showHide('#message', '#result');});
     $('.menu li').click(function(){
         $this = $(this);
         MESSAGE = $('span', $this).text();
         contactValentine();
     });
+
+    if(document.location.href.split('?').length > 1 && document.location.href.split('?')[1].match('Gsj7pMbMpSQ')){TEST = true;}
+
+    if(!DEBUG){
+        $('#loader').hide();
+        $('#result').hide();
+        $('#message').hide();
+        $('#happy').hide();
+
+        $('body').css('overflow-x', 'hidden');
+    }
+
+    var audioElement = document.createElement('audio');
+    if(audioElement.canPlayType('audio/mpeg') == 'maybe' || audioElement.canPlayType('audio/mpeg') == 'probably'){
+        $audioElement = $(audioElement);
+
+        $audioElement.attr({
+            'volume': '.5',
+            'autoplay':'autoplay',
+            'src':TEST?'sound/test.mp3':'sound/amour.mp3',
+            'type':'audio/mpeg'
+        });
+
+        $('#audio-controls .mute').hide();
+
+        $('#audio-controls').click(function(){
+            if(audioElement.paused){
+                $('#audio-controls .mute').hide();
+                $('#audio-controls .unmute').show();
+                audioElement.play();
+            } else {
+                $('#audio-controls .mute').show();
+                $('#audio-controls .unmute').hide();
+                audioElement.pause();
+            }
+        });
+
+        audioElement.addEventListener('ended', function(){
+            console.log('audio ended')
+            audioElement.src = TEST?'sound/test.mp3':'sound/amour.mp3';
+            audioElement.play();
+        });
+
+
+        $('body').append(audioElement);
+    } else {
+        $('#audio-controls').hide();
+    }
 });
-
-window.fbAsyncInit = function() {
-	console.log('FB init')
-    FB.init({
-      appId      : '374030012622491', // App ID
-      channelUrl : 'http://vmlfbval.appspot.com/channel.html', // Channel File
-      cookie     : true, // enable cookies to allow the server to access the session
-      xfbml      : true  // parse XFBML
-    });
-
-	FB.Event.subscribe('auth.statusChange', fbLoginStatus);
-};
 
 function fbLoginStatus(response) {
     if (response.authResponse) {
-        //user is already logged in and connected
         console.log("user is already logged in and connected");
-        $('.facebook-connect').show();
+        $('.fb-login-button').hide();
         $('.facebook-connect').click(function(){ countLikes() });
-    } else {
-        //user is not connected to your app or logged out
-        console.log("user not connected to app or logged out");
-        $('.facebook-login').click(function() {
-            console.log("button.onclick");
-            FB.login(function(response){
-                if (response.authResponse) {
-                    countLikes();
-                }else{
-                	console.log('user did not connect!')
-                } 
-            }, {scope:'read_stream'});
-        });
+        $('.facebook-connect').show()
     }
-};
+}
 
 function countLikes() {
     console.log('countLikes()')
+    showLoader();
+
     var user_id_counts = {};
     var highestCount = 0;
     var topLikers = [];
-    var topLikersInfo = [];
 	
 	FB.api(
     {
@@ -90,54 +137,109 @@ function countLikes() {
 		        query: 'SELECT uid, first_name, last_name, pic_big FROM user WHERE uid = ' + topLikers[0].toString()
 		    },
 		    function(data) {
-		    	//data = [];
 		    	if(data.length != 0){
-		    		var topName = 'Why not send ' + data[0].first_name + ' '+ data[0].last_name + ' a message?';
-		            var topId = data[0].uid;
+		    		var topName = data[0].first_name + ' '+ data[0].last_name + ' liked you ' + highestCount + ' time' + (highestCount > 1?'s':'');
 		            var topPic = data[0].pic_big;
+                    VALENTINE = data[0].uid;
+
+                    setResult(topName, topPic);
 		    	} else {
-		    		var topName = "Nobody likes you...";
-		    		var topPic = "img/" + Math.round(Math.random()*3) + ".jpeg";
+		    		var topName = "Yourself.";
+                    $('.text').text('You seem to be the only one who likes your posts. (Well, at least you can give yourself a big old hug!)');
+                    $('.facebook-continue').hide();
+                    var topPic;
+                    FB.api('/me/?fields=picture&type=large', function(response) {
+                        console.log(response);
+                        if(response && response.picture){
+                            topPic = response.picture;
+                        } else {
+                            topPic = "img/test.jpg";
+                        }
+
+                        window.setTimeout(function(){showHide('#happy', '#result')}, LIKE_DELAY * $('#loader li').length + NO_LIKE_DEALY);
+                        setResult(topName, topPic);
+                    });
 		    	}
 		    	
 		    	console.log(topName);
-		    	VALENTINE = data[0].uid;
-		    		
-		   		$('.frame img').hide().attr('src', topPic).bind('load', function(_e){
-		   			$target = $(_e.currentTarget);
-		   			$target.css('margin-top', 110 - $target.outerHeight()/2);
-		   			$target.show();
-		   		});
-		        $('.text').text(topName);
-
-				// $('#welcome').hide();
-				// $('#result').show();
+		    	
 	        }
 		)
 	});
 }
 
-function chooseMessage(){
-	//MESSAGE = $('.message-container p').text();
-	// $('#welcome').hide();
-	// $('#result').show();
+function setResult(_topName, _topPic){
+    _topPic = TEST?'img/test.jpeg':_topPic;
+
+    $('.name').text(_topName);
+    if(!DEBUG){ 
+        $('.frame img').attr('src', _topPic).bind('load', function(_e){
+            $('#result').css(
+                {
+                    'visibility': 'hidden',
+                    'position': 'absolute',
+                    'display':'block'
+                }
+            );
+            $target = $(_e.currentTarget);
+            $target.css('margin-top', 110 - $target.outerHeight()/2);
+
+            $('#result').css(
+                {
+                    'visibility': 'visible',
+                    'position': 'relative',
+                    'display':'none'
+                }
+            );
+        });
+    } else { //DEBUG ONLY
+        $('.frame img').attr('src', topPic).bind('load', function(_e){
+            $target = $(_e.currentTarget);
+            $target.css('margin-top', 110 - $target.outerHeight()/2);
+        });
+    }
+}
+
+function showLoader(){
+    showHide('#loader', '#welcome');
+
+    var $loaderli = $('#loader li');
+    $loaderli.each(function(_i, _this){
+        $this = $(_this);
+        $this.delay(LIKE_DELAY * _i).fadeIn(FADEIN_DELAY);
+
+        if(_i == $loaderli.length-1){
+            window.setTimeout(function(){showHide('#result', '#loader')}, LIKE_DELAY * $loaderli.length);
+        }
+    })
 }
 
 function contactValentine(){
+    console.log('contactValentine()');
 	FB.ui(
 		{
 			method:'send',
 			to: VALENTINE,
 			name: 'You\'re my Facebook Valentine!',
 			link: ROOT_URL, //CHANGE!!!
+            picture: ROOT_URL + 'img/app_icon.gif',
 			description: MESSAGE
-		}
-	);
-
-	//			picture: ROOT_URL + 'img/app_icon.gif',
+		},
+        function(response) {
+            if (response) {
+                console.log('message send!');
+                showHide('#happy', '#message');
+            } else {
+                console.log('message NOT send!');
+            }
+        }
+	);			
 }
 
-function showThankyou(){
-	// $('#welcome').hide();
-	// $('#result').show();
+function showHide(_show, _hide){
+    console.log('showHide(' + _show + ', ' + _hide + ')');
+    if(!DEBUG){
+        $(_hide).hide();
+        $(_show).fadeIn(FADEIN_DELAY); 
+    }
 }
